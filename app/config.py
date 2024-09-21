3fa86d5e47b0f4d48e0bba20aa426c8e86f1a579
offonyes <dimkakatran@gmail.com>
2024-09-21 23:53:20 +0400

    added config for .env

---
+++ app/config.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+
+URL_DATABASE = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
--- app/models/database.py
+++ app/models/database.py
@@ -1,21 +1,11 @@
-from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import DeclarativeBase
 
-engine = create_async_engine(
-    # "sqlite+aiosqlite:///database.sqlite3",
-    "postgresql+asyncpg://postgres:adminGrisha@localhost:5432/FastApi"
-)
+from app.config import URL_DATABASE
+
+engine = create_async_engine(URL_DATABASE)
 
 new_session = async_sessionmaker(engine, expire_on_commit=False)
-# URL_DATABASE = "sqlite+aiosqlite:///database.sqlite3"
-
-# engine = create_engine(URL_DATABASE)
-
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base = declarative_base()
 
 
 class BaseModel(DeclarativeBase):
